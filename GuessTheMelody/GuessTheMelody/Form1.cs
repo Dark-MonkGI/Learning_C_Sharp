763fd2b0a3e12567a85407aba5d465bec64c3de3
Dark-MonkGI <gryaznov774@gmail.com>
2022-03-29 22:55:17 +0400

    added first scren

---
+++ GuessTheMelody/GuessTheMelody/Form1.cs
@@ -0,0 +1,15 @@
+namespace GuessTheMelody
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
